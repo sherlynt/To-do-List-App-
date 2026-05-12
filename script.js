2ca1c522dacec312d19366238850677319aadf7d
sherlynt <tiwarisherlyn@gmail.com>
2026-05-12 07:59:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+//Select Dom Elements 
+const input = document.getElementById("todo-input")
+const addBtn = document.getElementById('add-btn')
+const list = document.getElementById('todo-list') 
+
+//Try to load saved todos from local storage
+const = saved localStorage.getItem('todos');
+const todos = saved? JSON.parsed(saved);
+
+
+function saveTodos() {
+    //this saves current todos to local storage
+
+}
